72ba532ce4a06af781fa15b0b24db2617969a11a
esraagad <israa.gad13@gmail.com>
2024-10-22 22:55:48 +0300

    Implement more details in webview screen.

--- app/src/main/java/com/example/greenopedia/utils/Constants.kt
+++ app/src/main/java/com/example/greenopedia/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.example.greenopedia.utils
 
 object Constants {
     const val API_BASE_URL = "https://trefle.io/api/"
-    const val APT_Token = "76SMaa8kUdWizI0GcI3kJbwoUKLVnRX_d8KC94v3XcE"
+    const val API_TOKEN = "76SMaa8kUdWizI0GcI3kJbwoUKLVnRX_d8KC94v3XcE"
     const val QUERY_PAGE_SIZE = 20
-
+    const val WIKIPIDIA_URL = "https://en.wikipedia.org/wiki/"
 }
